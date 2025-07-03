0493fc2e1d560a17f7cf8ad40de68c3456ceb95a
mohamednaguib-22 <mohamednageb20172@gmail.com>
2025-07-03 14:11:54 +0300

    chore: Sync Namespaces

--- ProductService/ProductService.Infrastructure/ServicesExtensions/ServicesRegistrationExtension.cs
+++ ProductService/ProductService.Infrastructure/ServicesExtensions/ServicesRegistrationExtension.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using ProductService.Application.Contract.IInfrastructure.IEventDispatcher;
 using ProductService.Application.Contract.IInfrastructure.IMessagePublisher;
 using ProductService.Infrastructure.Services.EventDispatcher;
 using ProductService.Infrastructure.Services.MessagePublisher;
+using ProductService.Infrastructure.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
